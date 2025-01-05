85993c451557fc3753b3ca96a70466dc2ea2f835
Veselin <vesselin60@gmail.com>
2025-01-05 15:44:01 +0200

    Intrepolation

--- link_js_toPage/script.js
+++ link_js_toPage/script.js
@@ -12,14 +12,21 @@
 // console.log(typeof(answer));
 // console.log(answer + 5);
 
-const answers = [];
+// const answers = [];
 
-answers[0] = prompt("Name?", "");
-answers[1] = prompt("Surname?", "");
-answers[2] = prompt("Age?", "");
+// answers[0] = prompt("Name?", "");
+// answers[1] = prompt("Surname?", "");
+// answers[2] = prompt("Age?", "");
 
-// document.write(answers);
-console.log(answers);
-console.log(typeof(answers));
-console.log(typeof(null));
+// // document.write(answers);
+// console.log(answers);
+// console.log(typeof(answers));
+// console.log(typeof(null));
 
+const category = "toys";
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Gocho";
+
+alert(`Hello ${user}`);
